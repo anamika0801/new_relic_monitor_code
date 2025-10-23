15f0874796557eb0f65c1ad13cecda1d961a96da
Anamika Pandey <156774804+anamika0801@users.noreply.github.com>
2025-10-24 01:06:45 +0530

    Add manifest.js for monitor configuration

---
+++ monitors/manifest.js
@@ -0,0 +1,8 @@
+[
+  {
+    "filePath": "monitors/ap-api-monitor.json",
+    "accountId": "7273540",
+    "apiKeySecretName": "NEW_RELIC_API_KEY",
+    "guid": "NzI3MzU0MHxTWU5USHxNT05JVE9SfGUyNDU3NTQ2LTNiMDMtNDRlMi1hMjBhLWNkYjhkYzk0OWYwNw"
+  },
+  ]
